e3b64cac675e7a0b97cc2dec3a4486151e09bb87
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2024-08-31 16:55:33 +0200

    quiet logs

--- images/freshrss/build.fish
+++ images/freshrss/build.fish
@@ -9,7 +9,7 @@ set fedora 40
 echo "Base Image | $fedora"
 echo "Tag        | $tag"
 echo "User ID    | $uid"
-wget -O /srv/FreshRSS.tar.gz (curl -s https://api.github.com/repos/FreshRSS/FreshRSS/releases/latest | jq -r ".tarball_url")
+wget --quiet -O /srv/FreshRSS.tar.gz (curl -s https://api.github.com/repos/FreshRSS/FreshRSS/releases/latest | jq -r ".tarball_url")
 and set ctr (buildah from --pull quay.io/fedora/fedora-minimal:$fedora)
 and buildah add $ctr /srv/FreshRSS.tar.gz /srv/
 and buildah copy $ctr ./src tmp
