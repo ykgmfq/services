f3d8b5ff0bb1cee3bb65a63cf1bf9ea39768be29
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2024-12-10 11:04:18 +0100

    Jellyfin Alpine 3.21

--- images/media/build.fish
+++ images/media/build.fish
@@ -4,13 +4,14 @@ function abort
     exit 1
 end
 set tag (basename (pwd))
-set alpine 3.20
+set alpine 3.21
 echo "Alpine | $alpine"
 echo "Tag    | $tag"
 set ctr (buildah from --pull docker.io/alpine:$alpine)
 and buildah config \
 --port 8096 \
 --env JELLYFIN_WEB_DIR=/usr/share/webapps/jellyfin-web \
+--env JELLYFIN_FFMPEG=/usr/lib/jellyfin-ffmpeg/ffmpeg \
 --env MALLOC_TRIM_THRESHOLD=131072 \
 --cmd /usr/bin/jellyfin \
 $ctr
