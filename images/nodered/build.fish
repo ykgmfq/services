74515ced7f1a439690c29d4bd727c7d6f12e360b
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2024-12-10 11:07:45 +0100

    Nodered use upstream image

--- images/nodered/build.fish
+++ images/nodered/build.fish
@@ -1,6 +1,14 @@
 #!/usr/bin/fish
+# https://regex101.com/r/xvHsoU/1
+function abort
+    buildah rm $argv
+    exit 1
+end
 set tag (basename (pwd))
-set nodejs 20
-echo "Tag | $tag"
-podman build --tag $tag --build-arg TAG_SUFFIX=minimal --build-arg NODE_VERSION=$nodejs node-red-docker/docker-custom
-
+set branch 4.0
+set ws 0.74
+set sun 2.1
+set ctr (buildah from --pull docker.io/nodered/node-red:$branch)
+and buildah run $ctr npm install node-red-contrib-{sun-position@$sun.x,home-assistant-websocket@$ws.x,timed-counter,spline-curve} passport-openidconnect
+and buildah commit --rm $ctr $tag
+or abort $ctr
