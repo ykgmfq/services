9ad4450bd9b2e4479d292f1df7acc2a98441a6e7
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2024-12-10 11:05:17 +0100

    Docs get x.1 branches

--- images/docs/build.fish
+++ images/docs/build.fish
@@ -9,8 +9,13 @@ set -x USERMAP_UID 879
 set -x USERMAP_GID $USERMAP_UID
 set -x PAPERLESS_LOGGING_DIR /tmp/log
 set url ghcr.io/paperless-ngx/paperless-ngx
-#set branch (podman search --list-tags --format "{{.Tag}}" --limit=300 $url | grep -P '^\d+.\d+$' | sort -V | tail -n 2 | head -1)
-set branch 2.11
+set tags (podman search --list-tags --format "{{.Tag}}" --limit=300 $url)
+set major (string collect $tags | grep --perl-regexp '^\d+.\d+$' | sort --version-sort | tail --lines 2)
+if contains "$major[2].1" $tags
+	set branch $major[2]
+else
+	set branch $major[1]
+end
 echo User ID for scan: $USERMAP_UID
 echo Branch: $branch
 set ctr (buildah from --pull $url:$branch)
