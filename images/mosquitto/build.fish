a4186bdcca1198467ef992296414bee602f74b40
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2026-05-17 17:53:29 +0000

    Fix alpine mosquitto startup

--- images/mosquitto/build.fish
+++ images/mosquitto/build.fish
@@ -8,6 +8,6 @@ set alpine 3
 set ctr (buildah from --pull docker.io/library/alpine:$alpine)
 and buildah run $ctr /sbin/apk add --no-progress --no-cache mosquitto mosquitto-clients
 and buildah copy $ctr mosquitto.conf /etc/mosquitto/mosquitto.conf
-and buildah config --user 1883:1883 --cmd mosquitto $ctr
+and buildah config --user 1883:1883 --cmd '["mosquitto", "-c", "/etc/mosquitto/mosquitto.conf"]' $ctr
 and buildah commit --rm $ctr $tag
 or abort $ctr
