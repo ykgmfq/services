f12368120e457b61fcece23f6d26292e63498286
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2026-06-10 18:36:46 +0000

    Revert "Confext mirgration"
    This reverts commit 7278e23499c977fa05367f0293a661c9a774d063.

--- sync.fish
+++ sync.fish
@@ -1,17 +1,6 @@
 #!/usr/bin/fish
-set confext /var/lib/confexts/services
-
-mkdir -p $confext/etc/containers/systemd $confext/etc/extension-release.d
-echo "ID=_any" > $confext/etc/extension-release.d/extension-release.services
-
-rclone sync systemd $confext/etc/containers/systemd
-
-if not systemd-confext refresh
-    exit 1
-end
-
+rclone sync systemd /etc/containers/systemd
 if not /usr/lib/systemd/system-generators/podman-system-generator -dryrun
     exit 1
 end
-
 systemctl daemon-reload
