a199b8dc428bf3c7c6795e36cbd9314fe2b693c4
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2026-06-27 09:37:39 +0000

    Move nodered auth to ingress

--- images/nodered/build.fish
+++ images/nodered/build.fish
@@ -11,6 +11,6 @@ set branch 4.1
 set ws 0.80
 set sun 2.1
 set ctr (buildah from --pull docker.io/nodered/node-red:$branch)
-and buildah run $ctr npm install node-red-contrib-{sun-position@$sun.x,home-assistant-websocket@$ws.x,timed-counter,spline-curve} passport-openidconnect
+and buildah run $ctr npm install node-red-contrib-{sun-position@$sun.x,home-assistant-websocket@$ws.x,timed-counter,spline-curve}
 and buildah commit --rm $ctr $tag
 or abort $ctr
