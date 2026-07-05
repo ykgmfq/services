71cb5399d04656123a5794dba81dc26f7927ed62
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2026-07-05 15:25:18 +0000

    Quiet sync

--- sync.fish
+++ sync.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/fish
-rclone sync systemd /etc/containers/systemd --exclude '*.md'
-if not /usr/lib/systemd/system-generators/podman-system-generator -dryrun
+rclone sync systemd /etc/containers/systemd --exclude '*.md' --quiet
+set generator_output (/usr/lib/systemd/system-generators/podman-system-generator -dryrun 2>&1)
+if test $status -ne 0
+    echo $generator_output
     exit 1
 end
 systemctl daemon-reload
